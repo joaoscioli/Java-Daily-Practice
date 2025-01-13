c3c29f25d64ee891bbaf0c978aba71509520dbd9
joaoscioli <joaoscioli@outlook.com>
2025-01-13 14:19:20 -0300

    feat (new project) add Task 2: Database Setup (PostgreSQL) main Day Three

---
+++ Day02/RestApiProject/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO students (name, email) VALUES ('John Doe', 'john.doe@example.com');
+INSERT INTO students (name, email) VALUES ('Jane Smith', 'jane.smith@example.com');
+INSERT INTO students (name, email) VALUES ('Alice Johnson', 'alice.johnson@example.com');
---
+++ Day02/RestApiProject/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
