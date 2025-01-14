67b76b939f39c46cf1de05f11828bf2ab918d7a9
joaoscioli <joaoscioli@outlook.com>
2025-01-14 14:06:00 -0300

    feat (new project) add second task for Day Four

---
+++ Day04/JwtAuthenticationProject/src/main/java/org/example/jwtauthenticationproject/JwtAuthenticationProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtauthenticationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationProjectApplication.class, args);
+	}
+
+}
---
+++ Day04/JwtAuthenticationProject/src/test/java/org/example/jwtauthenticationproject/JwtAuthenticationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtauthenticationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
