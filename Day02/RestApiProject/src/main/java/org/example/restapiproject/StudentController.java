10e52871658b088a294d7f8b2716cbcb51ee58d3
joaoscioli <joaoscioli@outlook.com>
2025-01-17 19:49:29 -0300

    doc (doc swagger) add documentation swagger patterns from project RestApi

--- Day02/RestApiProject/src/main/java/org/example/restapiproject/StudentController.java
+++ Day02/RestApiProject/src/main/java/org/example/restapiproject/StudentController.java
@@ -1,5 +1,7 @@
 package org.example.restapiproject;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@Tag(name = "User Controller", description = "Operations related to users")
 public class StudentController {
 
     private final StudentRepository studentRepository;
@@ -20,18 +23,21 @@ public class StudentController {
     }
 
     @PostMapping("/authenticate")
+    @Operation(summary = "Authenticate token", description = "Return new token to authenticate")
     public String authenticate(@RequestParam String username) {
         return jwtService.generateToken(username);
     }
 
     // GET /student
     @GetMapping()
+    @Operation(summary = "Get all students", description = "get all students from database")
     public List<Student> getAllStudent() {
         return studentRepository.findAll();
     }
 
     // POST /student/{id}
     @PostMapping
+    @Operation(summary = "Add new student", description = "Add new student to database")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         Student savedStudent = studentRepository.save(student);
         return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
@@ -39,6 +45,7 @@ public class StudentController {
 
     // DELETE /students/{id}
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete student to ID", description = "Delete from student with ID")
     public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
         studentRepository.deleteById(id);
         return ResponseEntity.noContent().build();
---
+++ Day02/RestApiProject/src/main/java/org/example/restapiproject/configuration/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package org.example.restapiproject.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("REST API Project")
+                        .version("1.0")
+                        .description("API for REST API Project")
+                .license(new License().name("Apache 2.0")
+                .url("http://www.apache.org/licenses/LICENSE-2.0.html")
+                ));
+
+    }
+}
