b605524bc41d8c4ae5224721adadcdb618879e74
joaoscioli <joaoscioli@outlook.com>
2025-02-04 19:36:14 -0300

    feat (product class) implement class and interface of product for project ecommerce-api

--- Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/model/Product.java
+++ Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/model/Product.java
@@ -1,4 +1,27 @@
 package org.example.ecommerceapi.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
 }
