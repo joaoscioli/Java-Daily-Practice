69e0ed974e831fc7206c72d6395ff467d981b363
joaoscioli <joaoscioli@outlook.com>
2025-02-13 21:36:21 -0300

    feat (Cart class) add Cart class domain.model to project ecommerce-api

--- Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/model/Cart.java
+++ Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/model/Cart.java
@@ -1,4 +1,25 @@
 package org.example.ecommerceapi.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany
+    private List<Product> products;
 }
