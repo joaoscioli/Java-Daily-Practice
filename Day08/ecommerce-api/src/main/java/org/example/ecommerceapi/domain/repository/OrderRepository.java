3ef6e7ac7dfeeed81f136819bf19fc2f5a5a8ccc
joaoscioli <joaoscioli@outlook.com>
2025-02-14 20:13:27 -0300

    feat (Repository interface) add OrderRepository domain.repository to project ecommerce-api

--- Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/repository/OrderRepository.java
+++ Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/repository/OrderRepository.java
@@ -1,4 +1,12 @@
 package org.example.ecommerceapi.domain.repository;
 
-public class OrderRepository {
+import org.example.ecommerceapi.domain.model.Order;
+import org.example.ecommerceapi.domain.model.User;
+
+import java.util.List;
+
+public interface OrderRepository {
+    List<Order> findByCustomer(User customer);
+    Order save(Order order);
+
 }
