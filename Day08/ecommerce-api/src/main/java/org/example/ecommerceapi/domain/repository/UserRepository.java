84f4e042a0933ac4156f890cb96e8de70a370a62
joaoscioli <joaoscioli@outlook.com>
2025-02-16 20:57:57 -0300

    feat (Repository interface) add UserRepository domain.repository to project ecommerce-api

--- Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/repository/UserRepository.java
+++ Day08/ecommerce-api/src/main/java/org/example/ecommerceapi/domain/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package org.example.ecommerceapi.domain.repository;
 
-public class UserRepository {
+import org.example.ecommerceapi.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    Optional<User> findByUsername(String email);
+    User save(User user);
 }
