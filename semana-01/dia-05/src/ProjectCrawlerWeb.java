a2f1f92b8825cb0c54e1bd52882fc2c5eec5dc65
joaoscioli <joaoscioli@outlook.com>
2025-05-15 20:11:41 -0300

    feat (final project) final project day 05 crawler web with Virtual Thread

--- semana-01/dia-05/src/ProjectCrawlerWeb.java
+++ semana-01/dia-05/src/ProjectCrawlerWeb.java
@@ -1,12 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ProjectCrawlerWeb {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         List<String> urls = List.of(
                 "https://oglobo.globo.com/blogs/a-hora-da-ciencia/",
                 "https://www.estadao.com.br/ultimas/",
@@ -14,23 +22,31 @@ public class ProjectCrawlerWeb {
                 "https://comunidade.tecnoblog.net/",
                 "https://noticias.uol.com.br/politica/"
         );
-
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.google.com/search?q=inurl:"))
-                .GET()
-                .build();
+        List<Thread> threads = new ArrayList<>();
 
-        CompletableFuture<HttpResponse<String>> future = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-        future.thenAccept(response -> {
-            System.out.println(response.statusCode());
-            System.out.println(response.body());
+        urls.forEach(url -> {
+            Thread vt = Thread.ofVirtual().start(() -> {
+                try {
+                    HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    Document doc = Jsoup.parse(response.body());
+                    String title = doc.select("title").text();
+                    System.out.println("URL: " + url + " | Título: " + title);
+                } catch (Exception e) {
+                    System.err.println("Erro ao processar " + url + ": " + e.getMessage());
+                }
+            });
+            threads.add(vt);
         });
-        future.join();
 
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.print("");
+    // Esperar todas as threads terminarem
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
