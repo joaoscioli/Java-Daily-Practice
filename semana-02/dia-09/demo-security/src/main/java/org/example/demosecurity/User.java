9efe86b3f60478bcaf51de312d6506d9577a294a
joaoscioli <joaoscioli@outlook.com>
2025-06-03 22:18:41 -0300

    feat(new project) init project dia 09

---
+++ semana-02/dia-09/demo-security/src/main/java/org/example/demosecurity/User.java
@@ -0,0 +1,12 @@
+package org.example.demosecurity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    @Column
+    String name;
+
+
+}
---
+++ semana-02/dia-09/demo-security/src/test/java/org/example/demosecurity/DemoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
