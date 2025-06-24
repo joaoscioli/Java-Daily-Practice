2ee0f8a64132e55022a863c95c30d6ab639015e1
joaoscioli <joaoscioli@outlook.com>
2025-06-24 20:08:15 -0300

    feat(user class) add features to User class to project dia 09

--- semana-02/dia-09/demo-security/src/main/java/org/example/demosecurity/User.java
+++ semana-02/dia-09/demo-security/src/main/java/org/example/demosecurity/User.java
@@ -1,12 +1,33 @@
 package org.example.demosecurity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private List<String> roles = new ArrayList<>();
+
     @Column
-    String name;
-
-
+    private boolean enabled = true;
 }
